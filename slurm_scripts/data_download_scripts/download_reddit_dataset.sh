f7ee9b262372d89188a61b42736e345d07a308fc
klh5 <k.awtycarroll@turing.ac.uk>
2025-07-14 12:02:17 +0100

    Add project root to slurm scripts

--- slurm_scripts/data_download_scripts/download_reddit_dataset.sh
+++ slurm_scripts/data_download_scripts/download_reddit_dataset.sh
@@ -13,11 +13,12 @@ module load baskerville
 module load bask-apps/live/live
 module load Python/3.10.8-GCCcore-12.2.0
 
+PROJECT_ROOT="/bask/projects/v/vjgo8416-tigers/ARC-TIGERS"
+
+source "$PROJECT_ROOT/.venv/bin/activate"
 
 # change working directory
-cd /bask/projects/v/vjgo8416-tigers/ARC-TIGERS
-
-source /bask/projects/v/vjgo8416-tigers/ARC-TIGERS/.venv/bin/activate
+cd $PROJECT_ROOT
 
 # change huggingface cache to be in project dir rather than user home
 export HF_HOME="/bask/projects/v/vjgo8416-tigers/hf_cache"
